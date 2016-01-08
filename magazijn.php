75376113e60de5573e9e58f68bd509a488792274
Coen <coen_visser@live.nl>
2016-01-08 09:18:20 +0100

    Upload my part

---
+++ magazijn.php
@@ -0,0 +1,53 @@
+<h1>VOORBEELD!!!</h1>
+
+<?php
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	
+	if ($conn->connect_error) {
+	    die("Connection failed: " . $conn->connect_error);
+	} 
+	
+	$sql = "SELECT 
+	    bedrijfsnaam,
+	    bestelregel.ordernummer,
+	    artikelcode,
+	    aantal,
+	    statusBeschrijving
+	FROM
+	    bestelling
+	        JOIN
+	    bestelregel ON bestelling.ordernummer = bestelregel.ordernummer
+	WHERE
+	    statusBeschrijving NOT LIKE 'verzonden'
+	        && bedrijfsnaam LIKE '%%';";
+	$result = $conn->query($sql);
+	
+	echo 
+	"<table>
+		<tr>
+			<th>Bedrijfsnaam</th>
+			<th>Ordernummer</th>
+			<th>Artikelcode</th>
+			<th>Aantal</th>
+			<th>Status</th>
+		</tr>";
+	
+	if ($result->num_rows > 0) {
+	    while($row = $result->fetch_assoc()) {
+	    	echo 
+	    	"<tr>
+	    		<td>" . $row['bedrijfsnaam'] . "</td>
+	    		<td>" . $row['ordernummer'] . "</td>
+	    		<td>" . $row['artikelcode'] . "</td>
+	    		<td>" . $row['aantal'] . "</td>
+	    		<td>" . $row['statusBeschrijving'] . "</td>
+	    	</tr>";
+	    }
+	} else {
+		echo "<td colspan='5' style='text-align: center'>Geen resultaten gevonden...</td>";
+	}
+	
+	echo "</table>";
+	
+	$conn->close();
+?>
--- verkoop.php
+++ verkoop.php
@@ -16,141 +16,22 @@
 	
 	echo 
 	'<form method="post" action="">
-		<fieldset class="bedrijfsgegevens">
-			<legend>Bedrijfsgegevens</legend>
-			<table>
-				<tr>
-					<th>
-						<label for="bedrijf">Bedrijf:
-					</th>
-					<th>
-						<select name="bedrijf">';
+		<label for="bedrijf">Bedrijf: 
+			<select name="bedrijf">';
 	
 	if ($result->num_rows > 0) {
 	    while($row = $result->fetch_assoc()) {
-	    	echo '				<option value="' . $row['bedrijfsnaam'] . '">' . $row['bedrijfsnaam'] . '</option>';
+	    	echo '<option value="' . $row['bedrijfsnaam'] . '">' . $row['bedrijfsnaam'] . '</option>';
 	    }
 	} else {
-		echo '					<option value="noresults">Geen resultaten gevonden...</option>';
+		echo '<option value="noresults">Geen resultaten gevonden...</option>';
 	}
 	
 	echo 
-	'						</select>
-						</label>
-						<input type="text" name="search" placeholder="Zoeken..." />
-					</th>
-				</tr>
-				<tr>
-					<td>
-						<label for="bedrijfsnaam">Bedrijfsnaam:
-					</td>
-					<td>
-						<input type="text" name="bedrijfsnaam" value="Appie" readonly></label>
-					</td>
-				</tr>
-				<tr>
-					<td>
-						<label for="straatnaam">Straatnaam:
-					</td>
-					<td>
-						<input type="text" name="straatnaam" value="Reactorweg" readonly></label>
-					</td>
-				</tr>
-				<tr>
-					<td>
-						<label for="huisnummer">Huisnummer:
-					</td>
-					<td>
-						 <input type="text" name="huisnummer" value="76" readonly></label><br />
-					</td>
-				</tr>
-				<tr>
-					<td>
-						<label for="woonplaats">Woonplaats:
-					</td>
-					<td>
-						<input type="text" name="woonplaats" value="Utrecht" readonly></label>
-					</td>
-				</tr>				
-			</table>
-		</fieldset><br />';
+	'		</select>
+		</label>
+		<input type="text" name="firstname" value="John" readonly>
+	</form>';
+	
+	$conn->close();
 ?>
-		<fieldset class="bestelling">
-		<legend>Bestelling</legend>
-			<table>
-				<tr>
-					<th>Medicijn</th>
-					<th>Aantal</th>
-					<th>Prijs</th>
-					<th>Totaalprijs</th>
-				</tr>
-			<?php
-				$sql = "SELECT 
-						    *
-						FROM
-						    medicijn
-						ORDER BY naam;";
-				
-				for ($i = 0; $i < 5; $i++) {
-					$result = $conn->query($sql);
-					echo'
-				<tr>
-					<td>
-						<select>';
-					if ($result->num_rows > 0) {
-					    while($row = $result->fetch_assoc()) {
-					    	echo
-					    	'<option value="' . $row['artikelcode'] . '">' . $row['naam'] . '</option>';
-					    }
-					} else {
-						echo '<option value="noresults">Geen resultaten gevonden...</option>';
-					}
-					
-					echo
-					'	</select>
-					</td>
-					<td><input type="number" name="XXXXXXX_aantal" min="0" max="99999" value="0"></td>
-					<td>&euro;XX,XX</td>
-					<td>&euro;XX,XX</td>
-				</tr>';
-				}
-			?>
-			<tr>
-				<th></th>
-				<th></th>
-				<th>Totaal (excl. korting):</th>
-				<th>&euro;XX,XX</th>
-			</tr>
-			<tr>
-				<td></td>
-				<td></td>
-				<td>Korting 5%:</td>
-				<td>&euro;XX,XX</td>
-			</tr>
-			<tr>
-				<th></th>
-				<th></th>
-				<th>Totaal (incl. korting):</th>
-				<th>&euro;XX,XX</th>
-			</tr>
-		</table>
-	</fieldset><br />
-	<fieldset>
-		<input class="submit" type="submit" name="submit" value="Bestellen" onclick="alert('Bestelling is doorgevoerd!')"/><br />
-	</fieldset>
-</form>
-
-<h3>Queries die gebruikt zijn:</h3>
-<p>SELECT <br />
-	    *<br />
-	FROM<br />
-	    klant<br />
-	ORDER BY bedrijfsnaam;</p>
-<p>Om de lijst van klanten te krijgen.</p>
-<br />
-<p>SELECT <br />
-	    *<br />
-	FROM<br />
-	    medicijn<br />
-	ORDER BY naam;</p>
-<p>Om de lijst van medicijnen te krijgen.</p>
