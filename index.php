a92977a52e9401c64fbef87dfb7e05b6eff21076
Steffion <steffion@icloud.com>
2016-01-08 11:44:58 +0100

    Revert "Upload my part"
    This reverts commit 75376113e60de5573e9e58f68bd509a488792274.

--- index.php
+++ index.php
@@ -20,7 +20,11 @@
 			
 			<nav>
 				<ul>
-					<li><a href="?page=home">Home</a></li>
+					<li><a href="?page=home">Home<b class="caret"></b></a>
+						<ul>
+							<li><a href="?page=voorbeeld">VOORBEELD</a></li>
+						</ul>
+					</li>
 					<li><a href="?page=verkoop">Verkoop</a></li>
 					<li><a href="?page=inkoop">Inkoop</a></li>
 					<li><a href="?page=facturering">Facturering</a></li>
@@ -30,8 +34,8 @@
 							<li><a href="#">Jaaromzet</a></li>
 							<li><a href="#">Meest winstgevend</a></li>
 							<li><a href="#">Meest verkocht</a></li>
-							<li><a href="#">Inkooptotalen</a></li>
-							<li><a href="#">Openstaande orders</a></li>
+							<li><a href="?page=inkooptotalen">Inkooptotalen</a></li>
+							<li><a href="?page=openstaandeorders">Openstaande orders</a></li>
 						</ul>
 					</li>
 				</ul>
